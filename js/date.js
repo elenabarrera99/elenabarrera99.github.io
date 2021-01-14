f745a994b0e18288b365e93d00fb7d16d3ee12be
elenabarrera99 <elenabarrera99@gmail.com>
2021-01-13 18:45:57 -0600

    css and Js

---
+++ js/date.js
@@ -0,0 +1,32 @@
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday", 
+    "Saturday"
+];
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+let d = new Date();
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let fullDate = dayName + ", " + monthName + " " + d.getDate() + ", " + d.getFullYear();
+
+document.getElementById('date').textContent = "Last Update: " + fullDate;
+
+let mod = new Date(document.lastModified);
+document.getElementById('OutputDate').textContent = mod;
